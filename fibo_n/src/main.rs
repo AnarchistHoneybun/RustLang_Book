c45e8346dc8231673c9372fde91d0a979bcb4210
Nirvana <thatonebipanda@gmail.com>
2023-02-11 03:13:00 +0530

    auto format

--- fibo_n/src/main.rs
+++ fibo_n/src/main.rs
@@ -5,20 +5,16 @@ fn main() {
 
     let mut n = String::new();
 
-    io::stdin()
-        .read_line(&mut n)
-        .expect("Failed to read line");
+    io::stdin().read_line(&mut n).expect("Failed to read line");
 
     let n: u32 = n.trim().parse().expect("Please type a number!");
 
     println!("The {}th Fibonacci number is {}", n, fibo(n));
-
-
 }
 
 fn fibo(n: u32) -> u32 {
-    let mut a =1;
-    let mut b =1;
+    let mut a = 1;
+    let mut b = 1;
     let mut result = 0;
 
     for _ in 0..n {
