3ef5bd3f53bc5d9bbf95aa3f7beaab8f37388dbe
sidd-ops <124734832+sidd-ops@users.noreply.github.com>
2023-02-12 01:01:49 +0530

    referencing a mutable variable

--- owner_ship/src/main.rs
+++ owner_ship/src/main.rs
@@ -1,12 +1,11 @@
 fn main() {
-    let s1 = String::from("buongiorno");
+    let mut s = String::from("hello");
 
-    let len = calculate_length(&s1);
+    change(&mut s);
 
-    println!("The length of '{}' is {}.", s1, len);
+    println!("{}", s);
 }
 
-fn calculate_length(s: &String) -> usize {
-    s.len() // len() returns the length of a String
-
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
