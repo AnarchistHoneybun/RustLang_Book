1463e1dd5f66a9867b5928ead067133a3aa10810
Nirvana <thatonebipanda@gmail.com>
2023-02-11 11:39:13 +0530

    cloning

--- owner_ship/src/main.rs
+++ owner_ship/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
     let s1 = String::from("Bing Chilling");
-    let s2 = s1;
+    // ! clone operations, however, are costly
+    let s2 = s1.clone();
+
+    println!("s1: {}, s2: {}", s1, s2);
     ////println!("s1: {}", s1);
 }
