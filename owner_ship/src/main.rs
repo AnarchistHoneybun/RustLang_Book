8216fb8d73bb929a3c5ed9ba3f4c9e6aeda1a993
sidd-ops <124734832+sidd-ops@users.noreply.github.com>
2023-02-11 23:33:19 +0530

    returning multiple values

--- owner_ship/src/main.rs
+++ owner_ship/src/main.rs
@@ -1,34 +1,13 @@
 fn main() {
+    let s1 = String::from("hello");
 
-    // gives_ownership moves its return value into s1
-    let s1 = gives_ownership();
-                                
-    // s2 comes into scope
-    let s2 = String::from("hello");
+    let (s2, len) = calculate_length(s1);
 
-    // s2 is moved into takes_and_gives_back, which also moves
-    // its return value into s3
-    let s3 = takes_and_gives_back(s2);
-} 
-// Here, s3 goes out of scope and is dropped. s2 was moved, so nothing
-// happens. s1 goes out of scope and is dropped.
-
-fn gives_ownership() -> String {
-    // gives_ownership will move its
-    // return value into the function
-    // that calls it
-
-    let some_string = String::from("yours"); // some_string comes into scope
-
-    some_string // some_string is returned and
-                // moves out to the calling
-                // function
+    println!("The length of '{}' is {}.", s2, len);
 }
 
-// This function takes a String and returns one
-fn takes_and_gives_back(a_string: String) -> String {
-    // a_string comes into
-    // scope
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() returns the length of a String
 
-    a_string // a_string is returned and moves out to the calling function
+    (s, length)
 }
