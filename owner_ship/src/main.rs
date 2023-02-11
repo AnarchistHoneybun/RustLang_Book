31a9105ee3181739a071828c7b92a2dfeb5e51e9
sidd-ops <124734832+sidd-ops@users.noreply.github.com>
2023-02-12 00:46:09 +0530

    passing by reference

--- owner_ship/src/main.rs
+++ owner_ship/src/main.rs
@@ -1,13 +1,12 @@
 fn main() {
     let s1 = String::from("buongiorno");
 
-    let (s2, len) = calculate_length(s1);
+    let len = calculate_length(&s1);
 
-    println!("The length of '{}' is {}.", s2, len);
+    println!("The length of '{}' is {}.", s1, len);
 }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len(); // len() returns the length of a String
+fn calculate_length(s: &String) -> usize {
+    s.len() // len() returns the length of a String
 
-    (s, length)
 }
