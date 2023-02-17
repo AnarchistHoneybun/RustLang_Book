df1f2e3f6f639f9b378c1e9e3c3edb02c62e1182
AnarchistHoneybun <74085528+AnarchistHoneybun@users.noreply.github.com>
2023-02-18 01:19:17 +0530

    tuple structs

--- structures/src/main.rs
+++ structures/src/main.rs
@@ -1,42 +1,6 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
-
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
 fn main() {
-    let user1 = User {
-        email: String::from("example@something.com"),
-        username: String::from("example"),
-        active: true,
-        sign_in_count: 1,
-    };
-
-    println!("User 1: {}", user1.email);
-
-    // TODO make another user using field init shorthand
-    let user2 = make_user(String::from("newexample@something.com"), String::from("newexample"));
-
-    println!("User 2: {}", user2.email);
-
-    // TODO make another user using struct update syntax
-
-    let user3 = User {
-        email: String::from("djkhaledanotherone@something.com"),
-        ..user1
-    };
-
-    println!("User 3: {}", user3.email);
-    
-    
-}
-
-fn make_user(email: String, username: String) -> User {
-    User {
-        email,
-        username,
-        active: true,
-        sign_in_count: 1,
-    }
+let black = Color(0, 0, 0);
+let origin = Point(0, 0, 0);
 }
