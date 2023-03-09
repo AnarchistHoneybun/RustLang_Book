9cb25eb7e514efe3f76d11f4b9b6eddffe5d122f
Nirvana <thatonebipanda@gmail.com>
2023-03-09 17:30:16 +0530

    struct refactoring; more well defined, though a bit verbose?

--- structures/src/main.rs
+++ structures/src/main.rs
@@ -1,17 +1,17 @@
 // todo area of rectangle
-// refactoring with tuples
+// // refactoring with tuples
+// refactoring with structs
 
-fn main(){
-
-    let rect1 = (30, 50);
-
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(rect1)
-    );
-
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+    println!("The area of the rectangle is {} square pixels.", area(&rect1));
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
