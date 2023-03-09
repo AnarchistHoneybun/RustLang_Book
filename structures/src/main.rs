7781af202a53106584a0d5ce964c380e68584156
Nirvana <thatonebipanda@gmail.com>
2023-03-09 18:05:30 +0530

    defining methods

--- structures/src/main.rs
+++ structures/src/main.rs
@@ -8,13 +8,21 @@ struct Rectangle {
     height: u32,
 }
 
-fn main() {
-    let rect1 = Rectangle { width: 30, height: 50 };
-
-    dbg!(&rect1);
-    println!("The area of the rectangle is {} square pixels.", area(&rect1));
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    dbg!(&rect1);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
 }
