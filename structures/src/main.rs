9b90da5460c0af1762b9323042a5ac0e35599784
Nirvana <thatonebipanda@gmail.com>
2023-03-09 17:12:36 +0530

    tuple refactoring

--- structures/src/main.rs
+++ structures/src/main.rs
@@ -1,16 +1,17 @@
 // todo area of rectangle
+// refactoring with tuples
 
 fn main(){
 
-    let length1 = 50;
-    let width1 = 30;
+    let rect1 = (30, 50);
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(length1, width1)
+        area(rect1)
     );
+
 }
 
-fn area(length: u32, width: u32) -> u32 {
-    length * width
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
