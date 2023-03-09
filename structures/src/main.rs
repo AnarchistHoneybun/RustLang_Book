43674bdd4cfbcec2e8eebdab9450881b3fd9818b
Nirvana <thatonebipanda@gmail.com>
2023-03-09 19:04:27 +0530

    using impl method

--- structures/src/main.rs
+++ structures/src/main.rs
@@ -12,6 +12,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
@@ -20,9 +24,23 @@ fn main() {
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 40,
+        height: 35,
+    };
+
     dbg!(&rect1);
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.area()
-    );
+    dbg!(&rect2);
+    dbg!(&rect3);
+
+    if rect1.can_hold(&rect3) {
+        println!("rect1 can hold rect3");
+    } else {
+        println!("rect1 can not hold rect3");
+    }
 }
